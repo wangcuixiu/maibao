ce74771da348a7f550d4397de408b102af9f2fc8
wangcuixiu <1161114040@qq.com>
2017-12-13 16:53:17 +0800

    这是php文件

--- php/checkUsername.php
+++ php/checkUsername.php
@@ -3,9 +3,9 @@
 $username = $_POST['username'];
 
 if($username === '15294165568') {
-    $ret = ['status' => 200, 'info' => '账号输入正确！'];
+    $ret = ['status' => 200, 'info' => '账号正确！'];
 } else {
-    $ret = ['status' => 401, 'info' => '账号输入错误！'];
+    $ret = ['status' => 401, 'info' => '账号有误！'];
 }
 
 echo json_encode($ret, JSON_UNESCAPED_UNICODE);
